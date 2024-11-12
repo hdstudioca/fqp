7a3b8c954d5fed889ce4170f710c5b9f0482bb38
Cade Dempsey <cadedempsey1@gmail.com>
2024-11-11 20:17:18 -0800

    initial commit

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom';
+import App from './App.jsx';
+import './index.css';
+
+const root = createRoot(document.body);
+root.render(<App />);
